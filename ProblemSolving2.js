4dcc8e927c7edce3cc021c8f90b2bd51df1fcdf2
Forrest M <Morio-Kun@MacInBlack.local>
2020-11-20 15:16:20 -0600

    PrimeSearcher is complete

--- ProblemSolving2.js
+++ ProblemSolving2.js
@@ -7,6 +7,38 @@ function isHappy(n) {
     else return isHappy(u);
 }
 
-let result = isHappy(4);
+let result = isHappy(16);
 console.log(result);
 
+result = isHappy(4);
+console.log(result);
+
+function primeCheck(integer) {
+    let isPrime = true;
+    if (integer === 1) {
+        console.log("1 is not prime")
+    }
+    else if (integer > 1){
+        for (let index = 2; index < integer; index++) {
+            if (integer % index == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            console.log(`${integer} is prime`);
+        } else {
+            //console.log(`${integer} is not prime`);
+        }
+    }
+    
+}
+
+function primeSearcher(repeatInt) {
+    for (let index = 0; index < repeatInt; index++) {
+        primeCheck(index);
+        
+    }
+}
+
+primeSearcher(100);
