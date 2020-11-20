a8ebdd52131e96a08777e7c7972022f6018ac91d
Forrest M <Morio-Kun@MacInBlack.local>
2020-11-20 14:27:50 -0600

    Solution to the happy number set

--- ProblemSolving2.js
+++ ProblemSolving2.js
@@ -1,4 +1,12 @@
 "use strict";
 
+function isHappy(n) {
+    if (n === 4) return false;
+    const u = n.toString().split('').reduce(((ac,cv) => ac + Math.pow(cv,2)), 0);
+    if (u === 1) return true;
+    else return isHappy(u);
+}
 
+let result = isHappy(4);
+console.log(result);
 
